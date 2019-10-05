7654fd7e6da568057f41a7c4489beb199bd2bbe1
RSibaja88 <54650378+RSibaja88@users.noreply.github.com>
2019-10-05 15:35:03 -0400

    everything but callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,3 +1,4 @@
+console.log("hw");
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
@@ -39,31 +40,39 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 */
 
 
-function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+function getLength(items, cb) {
+    // getLength passes the length of the array into the callback.
+    return cb(items.length);
 }
+//calling back function (local scope) into the global scope, where 'items' lives//
+const testGetLength = getLength(items, (length) => `${length}`);
+console.log(testGetLength);
 
-function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+function last(items, cb) {
+    // last passes the last item of the array into the callback.
+    return cb(items.length - 1);
 }
+const lastObject = last(items, (lastObject) => `${cb}`);
+console.log(lastObject);
 
-function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
-}
 
-function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
-}
+// function sumNums(x, y, cb) {
+//   // sumNums adds two numbers (x, y) and passes the result to the callback.
+// }
 
-function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
-}
+// function multiplyNums(x, y, cb) {
+//   // multiplyNums multiplies two numbers and passes the result to the callback.
+// }
 
-/* STRETCH PROBLEM */
+// function contains(item, list, cb) {
+//   // contains checks if an item is present inside of the given array/list.
+//   // Pass true to the callback if it is, otherwise pass false.
+// }
 
-function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
-}
+// /* STRETCH PROBLEM */
+
+// function removeDuplicates(array, cb) {
+//   // removeDuplicates removes all duplicate values from the given array.
+//   // Pass the duplicate free array to the callback function.
+//   // Do not mutate the original array.
+// }
